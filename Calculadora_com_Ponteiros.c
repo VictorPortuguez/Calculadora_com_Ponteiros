4d3f70b8b55f455fdc9627fad9f92badfed9fb0c
Victor-Portuguez <victorportuguez10@gmail.com>
2025-09-14 18:22:39 -0300

    Colocando em loop

--- Calculadora_com_Ponteiros.c
+++ Calculadora_com_Ponteiros.c
@@ -8,19 +8,20 @@ void menu(){
     printf("2- Subtracao:\n");
     printf("3- Multiplicacao:\n");
     printf("4- divisao:\n");
+    printf("5- Sair:\n");
     printf("Escolha 1 das opcoes:\n");
 }
 
 void soma(int *a, int *b, int *resultado){
-    *resultado = *a + *b;
+    printf("Resultado: %d\n",*resultado = *a + *b);
 }
 
 void subtracao(int *a, int *b, int *resultado){
-    *resultado = *a - *b;
+    printf("Resultado: %d\n",*resultado = *a - *b);
 }
 
 void multiplicacao(int *a, int *b, int *resultado){
-    *resultado = *a * *b;
+    printf("Resultado: %d\n",*resultado = *a * *b);
 }
 
 void divisao(int *a, int *b, int *resultado){
@@ -30,7 +31,7 @@ void divisao(int *a, int *b, int *resultado){
         printf("Divisao por 0 nao pode ser feita!\n");
     }
     else{
-        *resultado = *a / *b;
+        printf("Resultado: %d\n",*resultado = *a / *b);       
     }
     
 }
@@ -39,40 +40,46 @@ int main (){
 
     int num, num2, resultado, codigo;
 
-    printf("Digite o primeiro numero\n");
-    scanf("%d",&num);
+        printf("Digite o primeiro numero\n");
+        scanf("%d",&num);
 
-    printf("Digite o segundo numero\n");
-    scanf("%d",&num2);
+        printf("Digite o segundo numero\n");
+        scanf("%d",&num2);
+    
+    
+    do {
 
-    menu();
-    scanf("%d",&codigo);
+        menu();
+        scanf("%d",&codigo);
 
-    switch (codigo)
-    {
-        case 1:
-            soma(&num, &num2, &resultado);
-            printf("Resultado: %d\n", resultado);
-            break;
+        switch (codigo)
+        {
+            case 1:
+                soma(&num, &num2, &resultado);
+                break;
 
-        case 2:
-            subtracao(&num, &num2, &resultado);
-            printf("Resultado: %d\n", resultado);
-            break;
+            case 2:
+                subtracao(&num, &num2, &resultado);
+                break;
 
-        case 3:
-            multiplicacao(&num, &num2, &resultado);
-            printf("Resultado: %d\n", resultado);
-            break;
-         
-        case 4:
-            divisao(&num, &num2, &resultado);
-            printf("Resultado: %d\n", resultado);
-            break;
+            case 3:
+                multiplicacao(&num, &num2, &resultado);
+                break;
+            
+            case 4:
+                divisao(&num, &num2, &resultado);
+                break;
 
-        default:
-            break;
+                case 5:
+                printf("Saindo...\n");
+                break;
+
+            default:
+                printf("Opcao invalida\n");
+                break;
         }
+    
+    }while(codigo != 5);
 
-    return (0);
+return (0);
 }
