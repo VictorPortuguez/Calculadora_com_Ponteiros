79a1c1cca5cb8e5eb4a41b708a5812f58948a04b
Victor-Portuguez <victorportuguez10@gmail.com>
2025-09-14 17:49:50 -0300

    primeiro commit

---
+++ Calculadora_com_Ponteiros.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void menu(){
+
+    printf("O que gostaria de faze:\n");
+    printf("1- Somar:\n");
+    printf("2- Subtracao:\n");
+    printf("3- Multiplicacao:\n");
+    printf("4- divisao:\n");
+    printf("Escolha 1 das opcoes:\n");
+}
+
+void soma(int *a, int *b, int *resultado){
+    *resultado = *a + *b;
+}
+
+void subtracao(int *a, int *b, int *resultado){
+    *resultado = *a - *b;
+}
+
+void multiplicacao(int *a, int *b, int *resultado){
+    *resultado = *a * *b;
+}
+
+void divisao(int *a, int *b, int *resultado){
+
+    if (*b==0)
+    {
+        printf("Divisao por 0 nao pode ser feita!\n");
+    }
+    else{
+        *resultado = *a / *b;
+    }
+    
+}
+
+int main (){
+
+    int num, num2, resultado, codigo;
+
+    printf("Digite o primeiro numero\n");
+    scanf("%d",&num);
+
+    printf("Digite o segundo numero\n");
+    scanf("%d",&num2);
+
+    menu();
+    scanf("%d",&codigo);
+
+    switch (codigo)
+    {
+        case 1:
+            soma(&num, &num2, &resultado);
+            printf("Resultado: %d\n", resultado);
+            break;
+
+        case 2:
+            subtracao(&num, &num2, &resultado);
+            printf("Resultado: %d\n", resultado);
+            break;
+
+        case 3:
+            multiplicacao(&num, &num2, &resultado);
+            printf("Resultado: %d\n", resultado);
+            break;
+         
+        case 4:
+            divisao(&num, &num2, &resultado);
+            printf("Resultado: %d\n", resultado);
+            break;
+
+        default:
+            break;
+        }
+
+    return (0);
+}
